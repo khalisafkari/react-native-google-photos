0d15eda7958285c58fbc3e54034307dd7c327ed5
khalis afkari <khalis411@gmail.com>
2019-03-24 16:43:43 +0700

    add jspm io doc

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import item from './api'
 const Gphotos = item;
 export default Gphotos;
+
+
+//jspm.io cors allow
+// import('https://dev.jspm.io/react-native-google-photos')
+// .then(({ default: Gphotos }) => 
+// Gphotos('https://photos.google.com/share/AF1QipMTEPAiVF8t0YqLukflnOSQjwfd8ARIoT2h37AXvYO1uaWodbeiFoBUDuD_19tEbg/photo/AF1QipPA2Bq0JlAR9LoGD3mogsxSb9OZWEG4XqBDD4Rv?key=cjhUT0xrZjM5NGN2SVRLOVptZU5SMUlKV0lQYWpB')
+// .then(res=>{
+//     console.log(res)
+// }));
